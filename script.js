680f8ad6980a80836b410ffd0974f0e2fd2d1ff2
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-09 11:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let deg = 0;
+let line = document.getElementById("line");
+setInterval(()=> {
+	if(deg === 360) deg = 0;
+	line.style.transform = `rotate(${deg++}deg)`;
+}, 20);
